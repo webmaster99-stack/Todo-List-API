d6cba2c2e15ac934c1b9b92a5be3e17bd461fd56
Ilian Hadzhidimitrov <ilian_hadzhidimitrov99@abv.bg>
2025-10-16 12:11:50 +0300

    Alter todo model

--- todos/models.py
+++ todos/models.py
@@ -10,13 +10,13 @@ class Todo(models.Model):
         ("High", "High")
     ]
 
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    priority = models.CharField(choices=priority_choices, max_length=50)
+    title = models.CharField(max_length=50, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    priority = models.CharField(choices=priority_choices, max_length=50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_completed = models.BooleanField(default=False)
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="todos")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="todos", null=True, blank=True)
 
     def __str__(self):
         return self.title
